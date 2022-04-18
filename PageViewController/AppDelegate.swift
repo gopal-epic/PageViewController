bcb48ffb275d139a4d3ef2b6cdbf20359c93212e
Gopal Rao Gurram <gopal@getepic.com>
2022-04-18 17:42:08 -0400

    Deleted commented code

--- PageViewController/AppDelegate.swift
+++ PageViewController/AppDelegate.swift
@@ -13,28 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-//    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-//        
-//        if #available(iOS 13.0, *) {
-//            // In iOS 13 setup is done in SceneDelegate
-//        } else {
-//            let window = UIWindow(frame: UIScreen.main.bounds)
-//            self.window = window
-//        }
-//        
-//        return true
-//    }
-//    
-//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-//        if #available(iOS 13.0, *) {
-//            // In iOS 13 setup is done in SceneDelegate
-//        } else {
-//            self.window?.makeKeyAndVisible()
-//        }
-//        
-//        return true
-//    }
-
     // MARK: UISceneSession Lifecycle
     
     @available(iOS 13.0, *)
